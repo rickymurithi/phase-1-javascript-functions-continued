01ba6f94e2beee5522a2e44ae593eedec1475516
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-22 00:45:20 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// code
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun())
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(nester = '*') {
+    return function (hook = 'special') {
+        return `You are ${nester}${hook}${nester}!`
+    }
+}
+console.log(wrapAdjective())
